8088f02e9ed919dec4b51726374cbd1d866e7ca2
Steve Simpson <ssimpson64@gmail.com>
2024-02-07 17:27:04 +1300

    Fast-CRC: Bug fix. Use 0x5... rather than 0xA... for initial bit swap in all bit_reverse_x functions.

--- kernels/bit_reverse.c
+++ kernels/bit_reverse.c
@@ -18,9 +18,9 @@ static inline uint8_t bit_reverse_8(uint8_t const data)
 
   uint8_t const bit_swap_mask[] =
   {
-    0XAA,   // 1-bit pairs, ie. individual bits
+    0x55,   // 1-bit pairs, ie. individual bits
     0x33,   // 2-bit pairs,
-    0X0F,   // 4-bit pairs, ie. nibbles
+    0x0F,   // 4-bit pairs, ie. nibbles
   };
 
   result = (bit_swap_mask[0] & (result >> 1)) | ((result & bit_swap_mask[0]) << 1);
@@ -46,9 +46,9 @@ static inline uint16_t bit_reverse_16(uint16_t const data)
 
   uint16_t const bit_swap_mask[] =
   {
-    0XAAAA,   // 1-bit pairs, ie. individual bits
+    0x5555,   // 1-bit pairs, ie. individual bits
     0x3333,   // 2-bit pairs,
-    0X0F0F,   // 4-bit pairs, ie. nibbles
+    0x0F0F,   // 4-bit pairs, ie. nibbles
   };
 
   result = (bit_swap_mask[0] & (result >> 1)) | ((result & bit_swap_mask[0]) << 1);
@@ -83,9 +83,9 @@ static inline uint32_t bit_reverse_32(uint32_t const data)
 
   uint32_t const bit_swap_mask[] =
   {
-    0XAAAAAAAA,   // 1-bit pairs, ie. individual bits
+    0x55555555,   // 1-bit pairs, ie. individual bits
     0x33333333,   // 2-bit pairs,
-    0X0F0F0F0F,   // 4-bit pairs, ie. nibbles
+    0x0F0F0F0F,   // 4-bit pairs, ie. nibbles
   };
 
   result = (bit_swap_mask[0] & (result >> 1)) | ((result & bit_swap_mask[0]) << 1);
@@ -124,9 +124,9 @@ static inline uint64_t bit_reverse_64(uint64_t const data)
 
   uint64_t const bit_swap_mask[] =
   {
-    0XAAAAAAAAAAAAAAAA,   // 1-bit pairs, ie. individual bits
+    0x5555555555555555,   // 1-bit pairs, ie. individual bits
     0x3333333333333333,   // 2-bit pairs,
-    0X0F0F0F0F0F0F0F0F,   // 4-bit pairs, ie. nibbles
+    0x0F0F0F0F0F0F0F0F,   // 4-bit pairs, ie. nibbles
   };
 
   result = (bit_swap_mask[0] & (result >> 1)) | ((result & bit_swap_mask[0]) << 1);
