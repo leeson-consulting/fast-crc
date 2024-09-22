12db6dfa0547ec34995bc768ebb936f63501f9b3
culyun <leeson.consulting@gmail.com>
2024-09-22 21:20:14 +1200

    Added CUSTOM_TEST_CRC macro to allow for bespoke testing.
    Currently this takes the CRC polynomial degree, algorithm name, and the expected_crc value for the random test data:
    
    static const uint8_t RANDOM_TEST_DATA[] = {
        0x10, 0x51, 0x94, 0xf4, 0x46, 0x60, 0x63, 0x54, 0x53, 0x27, 0x53, 0x82, 0xef, 0x4e, 0x5f, 0x69,
        0x95, 0x4a, 0xde, 0xa8, 0xe6, 0x29, 0xeb, 0x7f, 0xe7, 0xe0, 0xba, 0xf5, 0xab, 0x0e, 0xe6, 0x3b,
        0x63, 0xc9, 0x10, 0x9d, 0xa3, 0x2e, 0x6b, 0x14, 0x15, 0x4f, 0x6a, 0x76, 0xbe, 0xba, 0xc6, 0x19,
        0x06, 0x86, 0xf7, 0xa7, 0x6f, 0x55, 0x8f, 0xe1, 0xad, 0xe5, 0x30, 0x66, 0x97, 0x1a, 0x9f, 0xfb
    };
    
    I'm using this to exercise the Fast CRC algorithm mini lookup tables, which have been provided by Dr. Nguyen.
    Since there's no way to corroborate these without expert review, I decided to simply do a value comparison
    with the standard lookup tables produced by pyCRC for the underlying polynomial.
    
    The current test still doesn't prove that every value in the table has been exercised,
    but this could be done with a little more effort.

--- test/test_crc.cpp
+++ test/test_crc.cpp
@@ -59,7 +59,7 @@ void test_crc(
   uint64_t crc = algorithm.crc_main(data_start, data_len);
 
   if (crc != expected_crc) {
-    ERROR("\n\n>>>   CRC Main Test Failed: Expected 0x%016lx , Actual 0x%016lx   <<<\n\n", crc_params.check, crc);
+    ERROR("\n\n>>>   CRC Main Test Failed: Expected 0x%016lx , Actual 0x%016lx   <<<\n\n", expected_crc, crc);
   }
 
   // 2. Test the cumulative CRC interface
@@ -82,7 +82,7 @@ void test_crc(
   crc = algorithm.crc_finish(crc, data, data_len - (data - data_start));
 
   if (crc != expected_crc) {
-    ERROR("\n\n>>>   CRC Accumulation Test #1 Failed: Expected 0x%016lx , Actual 0x%016lx   <<<\n\n", crc_params.check, crc);
+    ERROR("\n\n>>>   CRC Accumulation Test #1 Failed: Expected 0x%016lx , Actual 0x%016lx   <<<\n\n", expected_crc, crc);
   }
 
   // 2.2 Test ... with a 4, 3, 1, "rest of data" pass
@@ -101,15 +101,15 @@ void test_crc(
   crc = algorithm.crc_finish(crc, data, data_len - (data - data_start));
 
   if (crc != expected_crc) {
-    ERROR("\n\n>>>   CRC Accumulation Test #2 Failed: Expected 0x%016lx , Actual 0x%016lx   <<<\n\n", crc_params.check, crc);
+    ERROR("\n\n>>>   CRC Accumulation Test #2 Failed: Expected 0x%016lx , Actual 0x%016lx   <<<\n\n", expected_crc, crc);
   }
 
   printf("   ==>   PASS\n\n");
 }
 
-void short_test_crc(crc_parameters_t const & crc_params, CRC_Algorithm const algorithm)
+void test_crc_of_standard_check_string(crc_parameters_t const & crc_params, CRC_Algorithm const algorithm)
 {
-  printf("Short Test \"%s\" on \"%s\"", crc_params.name, CRC_CHECK_STRING);
+  printf("Test \"%s\" of \"%s\"", crc_params.name, CRC_CHECK_STRING);
 
   uint8_t const * const data_start = (uint8_t *) CRC_CHECK_STRING;
   size_t const data_len = CRC_CHECK_STRING_LEN;
@@ -126,12 +126,51 @@ constexpr uint64_t make_poly_mask(size_t const poly_degree)
   return (((uint64_t) 1) << poly_degree) - 1;
 }
 
-#define SHORT_TEST_CRC(poly_degree, crc_alg) short_test_crc(crc##poly_degree##_##crc_alg##_params, { \
+#define SHORT_TEST_CRC(poly_degree, crc_alg) test_crc_of_standard_check_string(crc##poly_degree##_##crc_alg##_params, { \
       .crc_main     = [](uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg(data, data_len) & make_poly_mask(poly_degree); }, \
       .crc_start    = [](uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg##_start(data, data_len) & make_poly_mask(poly_degree); }, \
       .crc_continue = [](uint64_t const crc, uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg##_continue(crc, data, data_len) & make_poly_mask(poly_degree); }, \
       .crc_finish   = [](uint64_t const crc, uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg##_finish(crc, data, data_len) & make_poly_mask(poly_degree); }, \
 }); \
+
+// RANDOM_TEST_DATA was produced from the following command:
+//
+//   sha512sum -b < <(echo -e "1234567890\x00")
+//
+// which produced
+//
+// 105194f44660635453275382ef4e5f69954adea8e629eb7fe7e0baf5ab0ee63b63c9109da32e6b14154f6a76bebac6190686f7a76f558fe1ade53066971a9ffb *-
+//
+
+static const uint8_t RANDOM_TEST_DATA[] = {
+    0x10, 0x51, 0x94, 0xf4, 0x46, 0x60, 0x63, 0x54, 0x53, 0x27, 0x53, 0x82, 0xef, 0x4e, 0x5f, 0x69,
+    0x95, 0x4a, 0xde, 0xa8, 0xe6, 0x29, 0xeb, 0x7f, 0xe7, 0xe0, 0xba, 0xf5, 0xab, 0x0e, 0xe6, 0x3b,
+    0x63, 0xc9, 0x10, 0x9d, 0xa3, 0x2e, 0x6b, 0x14, 0x15, 0x4f, 0x6a, 0x76, 0xbe, 0xba, 0xc6, 0x19,
+    0x06, 0x86, 0xf7, 0xa7, 0x6f, 0x55, 0x8f, 0xe1, 0xad, 0xe5, 0x30, 0x66, 0x97, 0x1a, 0x9f, 0xfb
+};
+
+static const size_t RANDOM_TEST_DATA_LEN = sizeof(RANDOM_TEST_DATA) / sizeof(RANDOM_TEST_DATA[0]);
+
+void test_crc_of_random_data(crc_parameters_t const & crc_params, CRC_Algorithm const algorithm, uint64_t const expected_crc)
+{
+  printf("Test \"%s\" of random test data", crc_params.name);
+
+  uint8_t const * const data_start = &RANDOM_TEST_DATA[0];
+  size_t const data_len = RANDOM_TEST_DATA_LEN;
+
+  return test_crc(crc_params, algorithm, data_start, data_len, expected_crc);
+}
+
+#define CUSTOM_TEST_CRC(poly_degree, crc_alg, expected_crc) test_crc_of_random_data( \
+    crc##poly_degree##_##crc_alg##_params, \
+    { \
+      .crc_main     = [](uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg(data, data_len) & make_poly_mask(poly_degree); }, \
+      .crc_start    = [](uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg##_start(data, data_len) & make_poly_mask(poly_degree); }, \
+      .crc_continue = [](uint64_t const crc, uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg##_continue(crc, data, data_len) & make_poly_mask(poly_degree); }, \
+      .crc_finish   = [](uint64_t const crc, uint8_t const * const data, size_t const data_len) -> uint64_t { return crc##poly_degree##_##crc_alg##_finish(crc, data, data_len) & make_poly_mask(poly_degree); }, \
+    }, \
+    expected_crc \
+); \
 
 ///////////////////////////////////////////////////////////////////////////////
 
